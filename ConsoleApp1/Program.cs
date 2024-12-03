df69d55b267801e632ab0cb252fee34075f80f4e
magamaz <ahmedovapatimat357@gmail.com>
2024-12-03 16:09:04 +0300

    первый commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ввести значениея A и B");
+            int A= Convert.ToInt32(Console.ReadLine());
+            int  B= Convert.ToInt32(Console.ReadLine());
+          
+            int P=(A+B)*2; 
+            int S=A*B;
+
+            Console.WriteLine($"вывести результат: {P}, {S}") ;
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
+
